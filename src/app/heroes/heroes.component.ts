dc8e6c1e650ba4dac55dd574b757e9050bee8ce8
Kevin Kabore <kevin.s.kabore@gmail.com>
2018-02-21 12:36:53 -0800

    Adds heroService with observable

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -14,7 +14,7 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
 
-  heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
 
@@ -22,10 +22,17 @@ export class HeroesComponent implements OnInit {
     this.selectedHero = hero
   }
 
-  constructor() {
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+
+  constructor(private heroService: HeroService) {
+
   }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
